3af24fbe9a3ae706e9f574a67abffb382aa300d0
minioncodes <aryan@digipants.com>
2024-11-06 17:23:44 +0530

    done with the google auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signin from "./pages/Sigin";  
+import Signin from "./pages/Sigin";
 import Signup from "./pages/signup";
 import Oauth from "./components/Oauth";
 import Home from "./pages/Home";
-
+import Header from "./components/Header";
+import Profile from "./pages/Profile";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/signin" element={<Signin />} /> 
-        <Route path='/signup' element={<Signup/>}/>
-        <Route path="/oauth" element={<Oauth/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/oauth" element={<Oauth />} />
+        <Route 
+          path="/profile" 
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          } 
+        />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div>
-     <h1 className="text-3xl  bg-red-900">Hmlo hmlo</h1>
+     <h1 className="text-3xl  bg-red-900">Pols aa gyi Pols</h1>
     </div>
   )
 }
