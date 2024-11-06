9b74f840cf50bdb13f7ce2f9a47ea10a22a672b9
minioncodes <aryan@digipants.com>
2024-11-06 11:56:29 +0530

    hmlo hmlo

--- frontend/src/components/Oauth.jsx
+++ frontend/src/components/Oauth.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
-import {siginStart,signInSuccess,signInFailure} from '../redux/user/userSlice.js'
-import {useDispatch} from 'react-redux'
+import {
+  siginStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice.js";
+import { useDispatch } from "react-redux";
 import { app } from "../firebase.js";
 
 export default function OAuth() {
-    const dispatch=useDispatch();
+  const dispatch = useDispatch();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
       console.log(result);
-      const res = await fetch("http://localhost:3000/user/gsignup", {
+      const res = await fetch("http://localhost:3000/user/gsignup/", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
         },
         body: JSON.stringify({
-          name: result.user?.displayName || "Anonymous", 
-          email: result.user?.email || "no-email@example.com", 
+          name: result.user?.displayName || "Anonymous",
+          email: result.user?.email || "no-email@example.com",
         }),
       });
-      const data=res.json();
+      const data =await res.json();
       dispatch(signInSuccess(data));
     } catch (err) {
       console.log("could not log with google", err);
